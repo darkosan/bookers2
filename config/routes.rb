d9472ad63d1808e4e96e78387baf45d571ff9003
Noriko Iida <lumos.5503@gmail.com>
2024-06-20 13:23:07 +0000

    6/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/create'
+  get 'books/index'
+  get 'books/show'
+  get 'homes/about' => 'homes#about', as: "about"
+  
+  root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
